3745c134fcb906bd377136078e5cdbb7bfbf0929
Nicolas <nicolas@gomez.cc>
2026-02-06 10:53:13 -0800

    Changes for CleanBC DX.

--- site-delete-unix.sh
+++ site-delete-unix.sh
@@ -15,17 +15,17 @@ if [ ! -z "${OC_ENV}" ] && [ ! -z "${OC_SITE_NAME}" ];  then
         oc delete secrets ${OC_SITE_NAME}-wordpress-secrets -n ${NAMESPACE}     
         echo "Applying Kustomize configuration"       
         # Inject namePrefix into kustomization.yaml
-        sed -i 's/namePrefix:.*$/namePrefix: '$OC_SITE_NAME'-/' ./deployments/kustomize/overlays/digital-${OC_ENV}/kustomization.yaml
-        sed -i 's/site:.*$/site: '$OC_SITE_NAME'/' ./deployments/kustomize/overlays/digital-${OC_ENV}/kustomization.yaml
-        sed -i 's/name: .*-wordpress-nginx/name: '$OC_SITE_NAME'-wordpress-nginx/' ./deployments/kustomize/overlays/digital-${OC_ENV}/patch.yaml        
-        sed -i 's/host: .*/host: '$OC_SITE_NAME'.apps.silver.devops.gov.bc.ca/' ./deployments/kustomize/overlays/digital-${OC_ENV}/patch.yaml
-        sed -i 's/APP_DOMAIN=.*$/APP_DOMAIN='$OC_SITE_NAME'.apps.silver.devops.gov.bc.ca/' ./deployments/kustomize/overlays/digital-${OC_ENV}/kustomization.yaml
-        sed -i 's/WORDPRESS_DB_HOST=.*$/WORDPRESS_DB_HOST='${OC_SITE_NAME}'-wordpress-mariadb/' ./deployments/kustomize/overlays/digital-${OC_ENV}/kustomization.yaml
-        sed -i 's/secretName: .*$/secretName: '${OC_SITE_NAME}'-wordpress-secrets/' ./deployments/kustomize/overlays/digital-${OC_ENV}/patch.yaml
+        sed -i 's/namePrefix:.*$/namePrefix: '$OC_SITE_NAME'-/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/kustomization.yaml
+        sed -i 's/site:.*$/site: '$OC_SITE_NAME'/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/kustomization.yaml
+        sed -i 's/name: .*-wordpress-nginx/name: '$OC_SITE_NAME'-wordpress-nginx/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/patch.yaml        
+        sed -i 's/host: .*/host: '$OC_SITE_NAME'.apps.gold.devops.gov.bc.ca/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/patch.yaml
+        sed -i 's/APP_DOMAIN=.*$/APP_DOMAIN='$OC_SITE_NAME'.apps.gold.devops.gov.bc.ca/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/kustomization.yaml
+        sed -i 's/WORDPRESS_DB_HOST=.*$/WORDPRESS_DB_HOST='${OC_SITE_NAME}'-wordpress-mariadb/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/kustomization.yaml
+        sed -i 's/secretName: .*$/secretName: '${OC_SITE_NAME}'-wordpress-secrets/' ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/patch.yaml
         # Inject namePrefix into patch.yaml
-        # sed -i.bak "s|^# *namePrefix:.*$|namePrefix: $OC_SITE_NAME-|g" ./deployments/kustomize/overlays/digital-${OC_ENV}/patch.yaml
-        oc delete -k ./deployments/kustomize/overlays/digital-${OC_ENV}
-        # mv -./deployments/kustomize/overlays/digital-${OC_ENV}-bak ./deployments/kustomize/overlays/digital-${OC_ENV}
+        # sed -i.bak "s|^# *namePrefix:.*$|namePrefix: $OC_SITE_NAME-|g" ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}/patch.yaml
+        oc delete -k ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}
+        # mv -./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}-bak ./deployments/kustomize/overlays/cleanbcdx-${OC_ENV}
 
     else
         printf >&2 "$(oc whoami)"
@@ -35,5 +35,5 @@ else
     echo >&2 'Current Variables'
     env | grep -i oc_
     echo >&2 ''
-    echo >&2 'Require variables to be set, run: export OC_ENV="test" OC_SITE_NAME="digital-dev"'
+    echo >&2 'Require variables to be set, run: export OC_ENV="test" OC_SITE_NAME="cleanbcdx-bh-test"'
 fi
